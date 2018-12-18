41024e27e6e552f79fb914979a63adbba2f96c2e
Ayush <ayush7.sinha@gmail.com>
2018-12-18 09:21:07 -0600

    Using Sublime now

---
+++ sourceFiles/bookstore.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include"Sales_item.h"
+
+int main()
+{
+	Sales_item curr;
+	
+	return 0;
+}
--- sourceFiles/test.cpp
+++ sourceFiles/test.cpp
@@ -2,9 +2,17 @@
 #include"Sales_item.h"
 int main(){
 	int v1 = 2, v2 = 3;
+	char yn;
 	std::cout<<"The sum of "<<v1
 		 <<" and "<<v2
 		 <<" is "<<v1+v2<<std::endl;
-
+	std::cout<<"Does this work?"<<std::endl;
+	std::cin>>yn;
+	if (yn == 'y'){
+		std::cout<<"Yay!"<<std::endl;
+	}
+	else{
+		std::cout<<"Oh no!"<<std::endl;
+	}
 	return 0;
 }
