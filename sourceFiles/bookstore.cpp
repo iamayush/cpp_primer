275791542cfcacee35698c2d58335ca901e55dc4
Ayush <ayush7.sinha@gmail.com>
2018-12-21 23:05:37 -0600

    Adding functionality for bookstore

--- sourceFiles/bookstore.cpp
+++ sourceFiles/bookstore.cpp
@@ -3,7 +3,21 @@
 
 int main()
 {
-	Sales_item curr;
+	Sales_item curr, new_trans; // curr and new transaction
+	std::cin>>curr; // first transaction
+
+	while(std::cin>>new_trans){
+		if(curr.isbn() == new_trans.isbn()){
+			curr += new_trans;
+		}
+		else{
+			std::cout<<"isbn must be same bro"<<std::endl;
+			return -1;
+		}
+	}
+
+	// Print curr (total) isbn, # copies, revenue, avg price
+	std::cout<<curr<<std::endl;
 	
 	return 0;
 }
